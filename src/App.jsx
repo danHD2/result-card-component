b8189ea447902f36a2d21f16549cdeae221234ee
unknown <dan.harabagiu.dimitrescu@gmail.com>
2024-10-26 15:21:54 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Result from './result'
 import Summary from './summary'
+import data from '../data.json'
 import './App.css'
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
   return (
     <>
       <div className='myCard drop-shadow-2xl flex items-center justify-center min-h-screen font-HankenGrotesk'>
+       <div className='flex flex-col w-full sm:w-auto sm:flex-row shadow-2xl shadow-cobaltBlueTwo rounded-3xl'>
        <Result />
-        <Summary />
+        <Summary data={data}/>
+       </div>
       
         
       </div>
--- src/summary.jsx
+++ src/summary.jsx
@@ -1,25 +1,33 @@
-import data from '../data.json'
 
-const colors = ['lightRed', "orangeyYellow", "greenTeal", "cobaltBlue"];
 
-function Summary() {
+const colors = ['text-lightRed', "text-orangeyYellow", "text-greenTeal", "text-cobaltBlue"];
+const bgColors = ['bg-lightRedTwo', "bg-orangeyYellowTwo", "bg-greenTealTwo", "bg-cobaltBlueTwo"]
+
+function Summary({ data }) {
     return (
-        <div className='summaryDiv p-12'>
-          <h2 className='summary text-xl font-semibold'>Summary</h2>
+        <div className='summaryDiv sm:w-80 h-full p-5 sm:p-12 flex flex-col'>
+          <div>
+          <h2 className='summary text-xl font-semibold pb-5'>Summary</h2>
           <div className='tabel'>
           {data.map((item, index) => (
-            <div key={index} className={`p-2 flex rounded bg-lightRedTwo`}>
+            <div key={index} className={`p-2 flex my-4 justify-between rounded-lg ${bgColors[index]}`}>
+              <div className='info flex'>
                 <img className='pr-4' src={item.icon} alt="icon" />
-                <p className={`text-${colors[index]}`} >{item.category}</p>
-                <p>{item.score} / 100</p>
+                <p className={`${colors[index]}`} >{item.category}</p>
+              </div>
+              <div>
+                <strong className='text-darkGrayBlue opacity-100'>{item.score}</strong><strong className='text-darkGrayBlue opacity-50'> / 100</strong>
+              </div>
             </div>
-          ))}
-                  
+          ))}                  
           </div>
-          <button className='button'>Continue</button>
+          </div>
+          <button className='button bg-darkGrayBlue text-white p-3 mt-5 w-full rounded-full hover:bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue'>Continue</button>
         </div>
     )
 }
+
+
 
 export default Summary
 
